88c23dda658307978f83aebb7278a7c9e6b74c3e
abinshihab <abushihab_330@hotmail.com>
2022-06-29 21:28:51 +0400

    commit to main

--- backend/api/Counter.cs
+++ backend/api/Counter.cs
@@ -2,13 +2,12 @@ using Newtonsoft.Json;
 
 namespace Company.Function
 {
-
-public class Counter 
-{
-[JsonProperty(PropertyName="id")]
-    public string id {get;set;}
-    [JsonProperty(PropertyName ="count")]
-    public int Count {get;set;}
-}
-
+    public class Counter 
+    {
+        [JsonProperty(PropertyName="id")]
+        public string Id {get; set;}
+        
+        [JsonProperty(PropertyName = "count")]
+        public int Count {get;set;}
+    }
 }
