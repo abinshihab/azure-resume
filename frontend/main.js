cdf48b0f069b60465f150887134fb036b24eef27
abinshihab <abushihab_330@hotmail.com>
2022-07-05 22:43:49 +0400

    commit to main

--- frontend/main.js
+++ frontend/main.js
@@ -1,22 +1,25 @@
-window.addEventListener('DOMContentLoaded', (event) => {
+window.addEventListener('DOMContentLoaded',even =>{
     getVisitCount();
-});
+})
 
-
-const functionApi = 'http://localhost:7071/api/GetAzureResumeCounter';
+const functionApi = "http://localhost:7071/api/GetAzureResumeCounter";
 
 const getVisitCount = () => {
-    let count = 30;
-    fetch(functionApi)
-    .then(response => {
-        return response.json()
-    })
-    .then(response => {
-        console.log("Website called function API.");
+
+    let count = 30; 
+    fetch(functionApi).then(response => {
+
+       return response.json 
+    }).then(response => {
+
+        console.log("Website called functionAPI,");
         count = response.count;
-        document.getElementById('counter').innerText = count;
-    }).catch(function(error) {
+        document.getElementById("counter").innerText= count;
+    }).catch(function(error){
+
         console.log(error);
-      });
+
+    });
     return count;
+
 }
