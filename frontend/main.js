5caf884b25b7c5319778677b47fea57db0954150
abinshihab <abushihab_330@hotmail.com>
2022-07-05 13:18:57 +0400

    Created fuction azure api

--- frontend/main.js
+++ frontend/main.js
@@ -1,25 +1,22 @@
-window.addEventListener('DOMContentLoaded',even =>{
+window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
-})
+});
 
-const functionApi = "http://localhost:7071/api/GetAzureResumeCounter";
+
+const functionApi = 'http://localhost:7071/api/GetAzureResumeCounter';
 
 const getVisitCount = () => {
-
-    let count = 30 ; 
-    fetch(functionApi).then(response => {
-
-        response.json 
-    }).then(response => {
-
-        console.log("Website called functionAPI,");
+    let count = 30;
+    fetch(functionApi)
+    .then(response => {
+        return response.json()
+    })
+    .then(response => {
+        console.log("Website called function API.");
         count = response.count;
-        document.getElementById("counter").innerText= count ;
-    }).catch(function(error){
-
+        document.getElementById('counter').innerText = count;
+    }).catch(function(error) {
         console.log(error);
-
-    });
+      });
     return count;
-
 }
